7cba92cb17f35e14259b9d94c47cde8469b8cf36
sahil kashetwar <kashetwartechlab@gmail.com>
2021-02-04 08:43:05 +0530

    angular project

---
+++ day-04/notes.js
@@ -0,0 +1,24 @@
+/** 
+ * Angular - 11
+ * 
+ *  - angular is framework design & developed by google and it's open source
+ *  - also there is huge community for support
+ *  - Once you have used anuglar 
+ *         - all setup for the project is automatically done by angular  
+ *         - so to speed up the development on actual application we can use angular 
+ * 
+*/
+
+// Angular | React Js | VueJs | Svelte few more.
+/*  
+ - who has developed
+ - how big support community if we got stuck 
+ - is this project is having short deadline means minmal setup 
+ - how is the need performance / which is good option for implementing performanant.
+*/
+
+// how to start on angular project.
+/// angular cli - cli.angular.io
+
+
+
