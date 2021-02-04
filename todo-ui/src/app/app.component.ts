9946a9af571b9a5340f8fa72ee386243e5832145
sahil kashetwar <kashetwartechlab@gmail.com>
2021-02-04 10:05:39 +0530

    actions for greeting message

--- todo-ui/src/app/app.component.ts
+++ todo-ui/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-ui';
+  userName = '';
+
+  changeName(userName: string) {
+    console.log(userName);
+    this.userName = userName;
+  }
 }
