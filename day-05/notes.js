72447a22b63dfec423260f87ec2043f7e593ffcb
sahil kashetwar <kashetwartechlab@gmail.com>
2021-02-05 10:18:01 +0530

    Day-05 Directives / userlist sample data

---
+++ day-05/notes.js
@@ -0,0 +1,65 @@
+
+/**
+ * Data Binding
+ *  - it's way to bind the data to an html
+ *  - also bind an event to markup
+ *  - to listen to the change we can
+ *      (click) / (change) / (blur) /
+ *  - to pass the value we can []
+*/
+
+// Day - 5
+/**
+ * Directive in angular js.
+ *  - directive something which uses to modify the HTML markup of DOM (Document object model)
+ *  - there 2 types of directive in angular
+ * 1. Attribute directives
+ *    - something which change markup base on attributes
+ *      - ngClass
+ *      - ngStyle
+ *      - ngModel - FormsModule
+ * 2. Structural Directives  (*)
+ *    - something which changes structure of an html
+ *      - *ngIf
+ *      - *ngFor
+ *      - *ngSwitch - assignment to explore
+ *
+*/
+
+/*
+    switch(userName) {
+        case: 1
+            console.log('hello 1')
+        case: 2
+            console.log('hello 1')
+        case: 3
+            console.log('hello 1')
+
+    }
+
+*/
+
+/*
+- truthy values --
+    "asdf",
+- falsy values  !! == false
+    "" , 0 , null, undefined, false
+*/
+
+
+
+// TASK - 05/02/2021
+/**
+ *
+ * https://www.json-generator.com/
+ *
+ * *ngFor to iterate over array
+ *     - make use ngClass
+ *           - score < 50 : danger background: red
+ *           - score > 50 : safe background: green
+ *     - make use ngStyle
+ *           - score >= 50 : user Name should be of size 30px
+ *     - make *ngIf
+ *          - show the city name only if it's available
+ *
+*/
--- todo-ui/src/app/app.component.ts
+++ todo-ui/src/app/app.component.ts
@@ -6,10 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  userName = '';
+  userName: string | null | undefined;
 
-  changeName(userName: string) {
-    console.log(userName);
+  buttonStyle: any = {};
+
+  constructor() {
+    this.userName = null;
+  }
+
+  onInputChange(e: any) {
+    console.log(e);
+  }
+
+  changeName(userName: string | null | undefined) {
+    if (userName === 'mango') {
+      this.buttonStyle = {
+        'font-size': '30px',
+        color: 'tomato',
+        background: 'white',
+      };
+    }
     this.userName = userName;
   }
 }
