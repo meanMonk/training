6f1bad0f8cbb35241903dcbb7ca7fc8a01cfe8ec
sahil kashetwar <kashetwartechlab@gmail.com>
2021-02-10 09:42:47 +0530

    ng style

--- todo-ui/src/app/components/directive-day-08/learn-directive/learn-directive.component.ts
+++ todo-ui/src/app/components/directive-day-08/learn-directive/learn-directive.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearnDirectiveComponent implements OnInit {
   selectedDirective: string = 'structural';
+  currenthoverElement: string = ''
   strDirectives: Array<string> = [
     'Ng If',
     'Ng for',
