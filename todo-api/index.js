bafb81c785c34e090b5df16abb35c5c8d8026e7d
sahil kashetwar <kashetwartechlab@gmail.com>
2021-02-22 09:59:37 +0530

    node express server

---
+++ todo-api/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3001;
+
+const users = [
+    {
+        name: 'Sam',
+        id: 'Sam',
+    },
+    {
+        name: 'Ram',
+        id: 'Ram',
+    }
+]
+
+app.get('/',(req,res) => {
+    res.status(200);
+    res.send({
+        message: "Welcome to express!!"
+    })
+})
+
+app.get('/loadusers',(req,res) => {
+    res.status(200);
+    res.send({
+        message: 'available alable users',
+        data: users
+    })
+})
+
+app.listen(PORT,() => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
