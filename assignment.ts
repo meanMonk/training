3296bfd619b42b7c81a4cd094011d993a4b5f22b
sahil kashetwar <kashetwartechlab@gmail.com>
2021-02-24 08:17:44 +0530

    changes for assignement

---
+++ assignment.ts
@@ -0,0 +1,27 @@
+/**
+ * Day 16: Assignment
+ *
+ *  - create one page for delivery management
+ *   - form to create parcel delivery request with back button if don't want fill reques
+ *      - sender name
+ *      - mobile
+ *      - address / city / state / pin
+ *
+ *      - reciever name
+ *      - mobile
+ *      - address / city / state / pin
+ *      - date
+ *      - goods:
+ *        - size 1kg : qnty : 50Rs
+ *        - size 2kg : qnty : 80Rs
+ *        - size 3kg : qnty : 125Rs
+ *      - Total disable field  rs
+ *      - submit
+ *
+ *
+ *   - list all the request in table or div
+ *     -  sender   > TOTAL COST
+ *     -  receiver
+ *     -  goods
+ *
+ */
